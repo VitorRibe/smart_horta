11498099a2825d05d43145272d8600b5b8301c1b
Vitor Ribeiro <vitorribeirodev@gmail.com>
2026-08-29 00:30:10 -0300

    Update irrigation time to 80 seconds for improved watering duration

--- src/smart_horta/config.h
+++ src/smart_horta/config.h
@@ -16,7 +16,7 @@ const long GMT = -3; // GMT (Brasil é "-3")
 
 // Definições de irrigação
 const int WATER_FLOW_RATE = 1; // Vazão em litros por minutos da bomba utilizada 
-const int IRRIGATION_TIME = 10; // Tempo que a bomba ficará ligada em Segundos
+const int IRRIGATION_TIME = 80; // Tempo que a bomba ficará ligada em Segundos
 inline bool PROGRAM_IRRIGATION = false; // guarda o estado da automatização da irrigação
 inline String IRRIGATION_HOUR = "00:00"; // variável que guarda o horário de irrigação
 
